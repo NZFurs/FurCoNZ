38cd4493af605641faccccb68e1eaf083280f6ee
NZSmartie <nzsmartie@gmail.com>
2020-01-29 22:39:20 +1300

    Simple check-in form

---
+++ src/FurCoNZ.Web/Areas/Admin/Controllers/CheckInController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FurCoNZ.Web.Services;
+using FurCoNZ.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FurCoNZ.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Policy = "AdminOnly")]
+    public class CheckInController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        public CheckInController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index(CancellationToken cancellationToken = default)
+        {
+            var tickets = await _orderService.GetDetailedAttendeeListAsync(includeExpiredOrders: false, cancellationToken);
+
+            return View(tickets.Select(t => new TicketDetailViewModel(t)));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CheckInTicket(int ticketId, CancellationToken cancellationToken = default)
+        {
+            await _orderService.CheckInTicketAsync(ticketId, cancellationToken); // Sets Ticket.CheckInTime (new column) to DateTimeOffset.Now()
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UndoCheckInTicket(int ticketId, CancellationToken cancellationToken = default)
+        {
+            await _orderService.UndoCheckInTicketAsync(ticketId, cancellationToken); // Sets Ticket.CheckInTime (new column) to null
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- src/FurCoNZ.Web/Services/IOrderService.cs
+++ src/FurCoNZ.Web/Services/IOrderService.cs
@@ -15,8 +15,10 @@ namespace FurCoNZ.Web.Services
         Task<IEnumerable<Order>> GetAllOrdersAsync(CancellationToken cancellationToken = default);
         Task<IEnumerable<Order>> GetUserOrdersAsync(User user, CancellationToken cancellationToken = default);
         Task<Order> GetUserPendingOrderAsync(User user, CancellationToken cancellationToken = default);
+        Task CheckInTicketAsync(int ticketId, CancellationToken cancellationToken);
         Task RefundFundsForOrderAsync(int orderId, int amountCents, string paymentProvider, string paymentReference, DateTimeOffset when, CancellationToken cancellationToken = default);
         Task<DateTimeOffset> ReserveTicketsForPurchaseAsync(IDictionary<int, int> ticketsToReserveById, CancellationToken cancellationToken = default);
+        Task UndoCheckInTicketAsync(int ticketId, CancellationToken cancellationToken);
         Task<Order> GetOrderByRef(int orderRef, CancellationToken cancellationToken = default);
         Task<Order> GetOrderById(int orderId, CancellationToken cancellationToken = default);
         Task<IEnumerable<Ticket>> GetDetailedAttendeeListAsync(bool includeExpiredOrders = false, CancellationToken cancellationToken = default);
